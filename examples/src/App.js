f534f2d777c60c6e47ff561799ce7110fbdffaac
Salvador Briones <salvador.briones@selectra.info>
2021-01-03 20:47:15 +0100

    switch functional component to class component (App.js)

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Salva', age: 30 },
       { name: 'Teodoro', age: 28 },
       { name: 'Eustaquio', age: 25 }
-    ]
-  });
+    ],
+    otherState: 'some other value'
+  };
 
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         {name: 'Salvador', age: 30},
         {name: 'Teodoro', age: 28},
@@ -21,16 +22,18 @@ const App = props => {
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hello word!</h1>
-      <p>Hello again!</p>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
-      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }/>
-      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello word!</h1>
+        <p>Hello again!</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }/>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
+      </div>
+    );
+  }
 }
 
 export default App;
